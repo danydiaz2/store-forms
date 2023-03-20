2d4ad3a1be8cd543130aed21ffb801a121b29598
dany diaz <daniel.diazg@sophossolutions.comdaniel diazdaniel>
2023-03-19 19:51:11 -0500

    Form control inputs

---
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-basic-form',
+  templateUrl: './basic-form.component.html',
+  styleUrls: ['./basic-form.component.scss']
+})
+export class BasicFormComponent implements OnInit {
+
+  nameField = new FormControl('', [Validators.required, Validators.maxLength(10)]);
+  emailField = new FormControl(); 
+  phoneField = new FormControl();
+  colorField = new FormControl();
+  dateField = new FormControl();
+  edadField = new FormControl();
+  
+  categoryField = new FormControl('category-2');
+  tagField = new FormControl();
+
+  agreeField = new FormControl(false);
+  genderField = new FormControl();
+  zoneField = new FormControl();
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.nameField.valueChanges
+      .subscribe( value => {
+        console.log(value);
+      });
+  }
+
+  getNameValue(){
+    console.log(this.nameField.value);
+  }
+
+  get isNameFieldValid() {
+    return this.nameField.touched && this.nameField.valid;
+  } 
+
+  get isNameFieldInvalid() {
+    return this.nameField.touched && this.nameField.invalid; 
+  }
+
+}
