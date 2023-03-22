1ea64c8cadc7ef79b0b4166d06b3b88fe2a1d3b5
dany diaz <daniel.diazg@sophossolutions.comdaniel diazdaniel>
2023-03-22 11:45:21 -0500

    categories form

--- src/app/admin/categories/components/category-form/category-form.component.ts
+++ src/app/admin/categories/components/category-form/category-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
+import {CategoriesService} from './../../../../core/services/categories.service';
 
 @Component({
   selector: 'app-category-form',
@@ -7,9 +12,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, 
+              private categoriesService: CategoriesService,
+              private router: Router,
+              private storage: AngularFireStorage) { 
+    this.buildForm();
+
+   }
 
   ngOnInit(): void {
+  }
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+      image: ['', Validators.required]
+    });
+  }
+
+  get nameField() {
+    return this.form.get('name');
+  }
+
+  get imageField() {
+    return this.form.get('image');
+  }
+
+  save(){
+    if (this.form.valid) {
+      this.createCategory();  
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+
+  private createCategory(){
+    const data = this.form.value;
+    this.categoriesService.createCategory(data)
+    .subscribe(rta => {
+      this.router.navigate(['/admin/categories']);
+    })
+  }
+
+  uploadFile(event) {
+    const image = event.target.files[0];
+    const name = 'category.png';
+    const ref = this.storage.ref(name);
+    const task = this.storage.upload(name, image);
+
+    task.snapshotChanges()
+    .pipe(
+      finalize(()=> {
+        const urlImage$ = ref.getDownloadURL();
+        urlImage$.subscribe(url => {
+          console.log(url);
+          this.imageField.setValue(url);
+        })
+      })
+    )
+    .subscribe();
   }
 
 }
